3559dc8a22ce85af4325f86e4716ee490c821cd3
haohu1 <>
2025-05-19 09:45:04 -0700

    Remove debug code while keeping Time to Resolution fix

--- scripts/debug-resolution.ts
+++
@@ -1,5 +0,0 @@
-import { debugDiarrheaResolutionTime } from '../models/stockAndFlowModel';
-
-console.log('Debugging diarrhea resolution time calculation...');
-debugDiarrheaResolutionTime();
-console.log('Debug complete.'); 
